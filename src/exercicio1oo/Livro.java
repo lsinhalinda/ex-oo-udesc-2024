bd6ac21eea5ce9e0a454a2653e09f69085857f0d
Isa <souzaisaihd@gmail.com>
2024-08-29 20:36:05 -0300

    Commit do livro - emprestar e devolver

---
+++ src/exercicio1oo/Livro.java
@@ -0,0 +1,18 @@
+package exercicio1oo;
+public class Livro {
+    public String nome, autor;
+    public boolean disponivel = true;
+
+    public boolean indisponivel() {
+        return disponivel;
+    }
+    public void retirado(){
+        disponivel=false;
+        System.out.println("Livro indisponivel para retirada");
+    }
+    public void devolver(){
+        disponivel=true;
+        System.out.println("Livro disponivel para retirada/emprestimo");
+    }
+
+}
---
+++ src/exercicio1oo/TestaLivro.java
@@ -0,0 +1,20 @@
+package exercicio1oo;
+public class TestaLivro {
+    public static void main(String[] args) {
+        Livro book = new Livro();
+        book.nome="A menina que roubava livros";
+        book.retirado();
+
+        if(book.indisponivel()){
+            System.out.println("Disponível para retirada!");
+        }else{
+            System.out.println("Indisponível para retirada");
+        }
+        book.devolver();
+        if(book.indisponivel()){
+            System.out.println("Disponível para retirada!");
+        }else{
+            System.out.println("Indisponível para retirada");
+        }
+    }
+}
