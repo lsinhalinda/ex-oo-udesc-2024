c4419b8f12bdb8843a8282a1cad230c8f7144792
Isa <souzaisaihd@gmail.com>
2024-08-29 20:12:05 -0300

    Commit incial retangulo

---
+++ src/exercicio1oo/Retangulo.java
@@ -0,0 +1,8 @@
+package exercicio1oo;
+
+public class Retangulo {
+    public double w, h;
+    public double calcularArea(){
+        return w*h;
+    }
+}
---
+++ src/exercicio1oo/TestaRetangulo.java
@@ -0,0 +1,11 @@
+package exercicio1oo;
+
+public class TestaRetangulo {
+    public static void main(String[] args) {
+        Retangulo Ret = new Retangulo();
+        Ret.h=9.0;
+        Ret.w=8.0;
+        System.out.println("Área: "+Ret.calcularArea());
+
+    }
+}
